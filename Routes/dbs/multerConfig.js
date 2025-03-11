0516c1ab1625a1313141ac83cb31ba9c62415b42
ayush gupta <aerospace141@gmail.com>
2025-03-11 13:10:16 +0530

    Update multerConfig.js

--- Routes/dbs/multerConfig.js
+++ Routes/dbs/multerConfig.js
@@ -2,14 +2,8 @@ const multer = require("multer");
 const path = require("path");
 
 // Storage setup (Temporary)
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null); // Temp save before uploading to Cloudinary
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
-});
+const storage = multer.memoryStorage(); 
+
 
 // File filter (only images)
 const fileFilter = (req, file, cb) => {
